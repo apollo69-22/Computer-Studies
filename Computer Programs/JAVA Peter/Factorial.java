9659f346c057fc0be888ba739e95388f8056d958
cyber-ai905 <84355434+cyber-ai905@users.noreply.github.com>
2022-12-08 04:41:55 -0800

    Updated Factorial.java

--- Computer Programs/JAVA Peter/Factorial.java
+++ Computer Programs/JAVA Peter/Factorial.java
@@ -4,7 +4,7 @@ class Factorial {
         System.out.print("Enter your Number: ");
         long num = Keyboard.readLong();
 
-        if (num <= 20) {
+        if (num <= 20 && num >= 0) {
             System.out.print("Your Answer is: " + num);
             for (long i = (num - 1); i > 0; i--) {
                 System.out.print("*" + i);
@@ -13,7 +13,11 @@ class Factorial {
 
             System.out.printf(" = %d", num);
         
-        } else
+        } 
+        
+        else if (num > 20)
             System.out.println("Buffer OVERFLOW OCCURS With Numbers ABOVE 20");
+        else if (num < 0)
+            System.out.println("Number cannot be smaller than 0");
     }
 }
