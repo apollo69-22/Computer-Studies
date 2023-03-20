ead40246617edcfe0504dd0fa83e5c08050ea934
apollo69-22 <113331394+apollo69-22@users.noreply.github.com>
2023-03-20 15:17:02 +0100

    Updated Final Project w/ all Planets

--- Computer Programs/java_peter/Year_Final_Project/Space-Traveler-Java-Game/SpaceTraveler.java
+++ Computer Programs/java_peter/Year_Final_Project/Space-Traveler-Java-Game/SpaceTraveler.java
@@ -18,6 +18,8 @@ class SpaceTraveler {
     public static final int dark_orange = 214;
     public static final int brown = 58;
     public static final int grn_skin = 10;
+    public static final int blue = 153;
+    public static final int dark_blue = 30;
 
     public static void drawCaptain() {
         System.out.println(ansi().fg(brown).a("                                                      **"));
@@ -71,31 +73,146 @@ class SpaceTraveler {
         System.out.println(ansi().fg(yellow).a("                                 ################################################"));
         System.out.println(ansi().fg(yellow).a("**************************************************************************************************************"));
     }
+
+    public static void drawMercury() {
+        System.out.println(ansi().fg(yellow).a("**************************************************************************************************************"));
+        System.out.println(ansi().fg(gray).a("                                                      #######"));
+        System.out.println(ansi().fg(gray).a("                                                    ###########"));
+        System.out.println(ansi().fg(gray).a("                                                  ###############"));
+        System.out.println(ansi().fg(gray).a("                                                  ###############"));
+        System.out.println(ansi().fg(gray).a("                                                    ###########"));
+        System.out.println(ansi().fg(gray).a("                                                      #######"));
+        System.out.println(ansi().fg(yellow).a("**************************************************************************************************************"));
+    }
+
+    public static void drawVenus() {
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+        System.out.println(ansi().fg(yellow).a("                                                      ######"));
+        System.out.println(ansi().fg(yellow).a("                                                   ############"));
+        System.out.println(ansi().fg(yellow).a("                                                 ################"));
+        System.out.println(ansi().fg(yellow).a("                                               ####################"));
+        System.out.println(ansi().fg(yellow).a("                                               ####################"));
+        System.out.println(ansi().fg(yellow).a("                                                 ################"));
+        System.out.println(ansi().fg(yellow).a("                                                   ############"));
+        System.out.println(ansi().fg(yellow).a("                                                      ######"));
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+    }
+
+    public static void drawEarth() {
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+        System.out.println(ansi().fg(blue).a("                                                        ######"));
+        System.out.println(ansi().fg(blue).a("                                                     ############"));
+        System.out.println(ansi().fg(grn_skin).a("                                                   ################"));
+        System.out.println(ansi().fg(grn_skin).a("                                                 ####################"));
+        System.out.println(ansi().fg(blue).a("                                                 ####################"));
+        System.out.println(ansi().fg(blue).a("                                                   ################"));
+        System.out.println(ansi().fg(blue).a("                                                     ############"));
+        System.out.println(ansi().fg(blue).a("                                                        ######"));
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+    }
+
+    public static void drawMars() {
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+        System.out.println(ansi().fg(dark_orange).a("                                                      #######"));
+        System.out.println(ansi().fg(dark_orange).a("                                                    ###########"));
+        System.out.println(ansi().fg(dark_orange).a("                                                  ###############"));
+        System.out.println(ansi().fg(dark_orange).a("                                                  ###############"));
+        System.out.println(ansi().fg(dark_orange).a("                                                    ###########"));
+        System.out.println(ansi().fg(dark_orange).a("                                                      #######"));
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+    }
     
     public static void drawJupiter() {
         System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
-        System.out.println(ansi().fg(gray).a("                                                    ##############"));
-        System.out.println(ansi().fg(gray).a("                                               ######################"));
-        System.out.println(ansi().fg(green_yellow).a("                                        ###################################"));
-        System.out.println(ansi().fg(white_yellow).a("                                       #######################################"));
-        System.out.println(ansi().fg(white_yellow).a("                                     ##########################################"));
-        System.out.println(ansi().fg(dark_orange).a("                                  ###############################################"));        
-        System.out.println(ansi().fg(orange).a("                                 ##################################################"));
-        System.out.println(ansi().fg(white_yellow).a("                                ####################################################"));
-        System.out.println(ansi().fg(orange).a("                                ####################################################"));
-        System.out.println(ansi().fg(dark_orange).a("                                ###################################################"));
-        System.out.println(ansi().fg(yellow).a("                                 #################################################"));
-        System.out.println(ansi().fg(yellow).a("                                   #############################################"));
-        System.out.println(ansi().fg(yellow).a("                                       #######################################"));
-        System.out.println(ansi().fg(yellow).a("                                             ############################"));
-        System.out.println(ansi().fg(yellow).a("                                               ######################"));
-        System.out.println(ansi().fg(yellow).a("                                                      ##########"));
-        System.out.println(ansi().fg(yellow).a("**************************************************************************************************************"));
+        System.out.println(ansi().fg(gray).a("                                                  ##############"));
+        System.out.println(ansi().fg(gray).a("                                               ####################"));
+        System.out.println(ansi().fg(green_yellow).a("                                          ##############################"));
+        System.out.println(ansi().fg(white_yellow).a("                                      ######################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                    ##########################################"));
+        System.out.println(ansi().fg(dark_orange).a("                                   ############################################"));        
+        System.out.println(ansi().fg(orange).a("                                  ##############################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                 ################################################"));
+        System.out.println(ansi().fg(orange).a("                                 ################################################"));
+        System.out.println(ansi().fg(dark_orange).a("                                 ################################################"));
+        System.out.println(ansi().fg(yellow).a("                                  ##############################################"));
+        System.out.println(ansi().fg(yellow).a("                                   ############################################"));
+        System.out.println(ansi().fg(yellow).a("                                      ######################################"));
+        System.out.println(ansi().fg(yellow).a("                                          ##############################"));
+        System.out.println(ansi().fg(yellow).a("                                               ####################"));
+        System.out.println(ansi().fg(yellow).a("                                                  ##############"));
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+    }
+
+    public static void drawSaturn() {
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+        System.out.println(ansi().fg(white_yellow).a("                                                  ##############"));
+        System.out.println(ansi().fg(white_yellow).a("                                               ####################"));
+        System.out.println(ansi().fg(white_yellow).a("                                          ##############################"));
+        System.out.println(ansi().fg(white_yellow).a("                                      ######################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                    ##########################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                   ############################################"));        
+        System.out.println(ansi().fg(white_yellow).a("                                  ##############################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                 ################################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                 ################################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                 ################################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                  ##############################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                   ############################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                      ######################################"));
+        System.out.println(ansi().fg(white_yellow).a("                                          ##############################"));
+        System.out.println(ansi().fg(white_yellow).a("                                               ####################"));
+        System.out.println(ansi().fg(white_yellow).a("                                                  ##############"));
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+    }
+
+    public static void drawUranus() {
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+        System.out.println(ansi().fg(blue).a("                                                    ##########"));
+        System.out.println(ansi().fg(blue).a("                                                 ################"));
+        System.out.println(ansi().fg(blue).a("                                               ####################"));
+        System.out.println(ansi().fg(blue).a("                                             ########################"));
+        System.out.println(ansi().fg(blue).a("                                             ########################"));
+        System.out.println(ansi().fg(blue).a("                                             ########################"));
+        System.out.println(ansi().fg(blue).a("                                               ####################"));
+        System.out.println(ansi().fg(blue).a("                                                 ################"));
+        System.out.println(ansi().fg(blue).a("                                                    ##########"));
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+    }
+
+    public static void drawNeptune() {
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
+        System.out.println(ansi().fg(dark_blue).a("                                                    ##########"));
+        System.out.println(ansi().fg(dark_blue).a("                                                 ################"));
+        System.out.println(ansi().fg(dark_blue).a("                                               ####################"));
+        System.out.println(ansi().fg(dark_blue).a("                                             ########################"));
+        System.out.println(ansi().fg(dark_blue).a("                                             ########################"));
+        System.out.println(ansi().fg(dark_blue).a("                                             ########################"));
+        System.out.println(ansi().fg(dark_blue).a("                                               ####################"));
+        System.out.println(ansi().fg(dark_blue).a("                                                 ################"));
+        System.out.println(ansi().fg(dark_blue).a("                                                    ##########"));
+        System.out.println(ansi().fg(gray).a("**************************************************************************************************************"));
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-        System.out.println(ansi().bg(BLACK));
+        //System.out.println(ansi().bg(BLACK));
+        drawCaptain();
+        System.out.println();
+        drawSun();
+        System.out.println();
+        drawMercury();
+        System.out.println();
+        drawVenus();
+        System.out.println();
+        drawEarth();
+        System.out.println();
+        drawMars();
+        System.out.println();
         drawJupiter();
+        System.out.println();
+        drawSaturn();
+        System.out.println();
+        drawUranus();
+        System.out.println();
+        drawNeptune();
     }
 }
